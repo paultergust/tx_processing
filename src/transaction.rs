0de713f3a0613c6d1c70359a007d78ed3d1ac418
paultergust <prmoreira98@gmail.com>
2024-08-27 03:37:01 +0100

    Refactor and add some comments

--- src/transaction.rs
+++ src/transaction.rs
@@ -86,6 +86,7 @@ impl<'de> Deserialize<'de> for TxType {
     }
 }
 
+// when amount is missing (disputes, resolves, chargebacks), default value is set to 0.0
 fn deserialize_amount<'de, D>(deserializer: D) -> Result<f32, D::Error>
 where
     D: serde::Deserializer<'de>,
