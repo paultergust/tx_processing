bd0bfd9893e650e653edebcaff48fef4bd3c91d6
paultergust <prmoreira98@gmail.com>
2024-08-27 03:20:07 +0100

    Fix bug where the under_dispute flag was not toggling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod transaction;
 mod account;
+mod transaction;
 
 use std::error::Error;
 use std::fs::remove_dir_all;
@@ -7,12 +7,12 @@ use std::{fs::File, io::BufReader};
 
 use clap::Parser;
 use csv::{ReaderBuilder, Trim, Writer};
-use sled::Db;
 use serde_json::{from_slice, to_string};
+use sled::Db;
 use transaction::TxType;
 
-use crate::transaction::Transaction;
 use crate::account::Account;
+use crate::transaction::Transaction;
 
 #[derive(Parser)]
 struct Cli {
@@ -33,7 +33,10 @@ fn process_transactions(filename: String) -> Result<(), Box<dyn Error>> {
     let ac_db = sled::open(Account::DB_NAME)?;
     let file = match File::open(filename) {
         Ok(v) => v,
-        _ => panic!("error opening file"),
+        _ => {
+            cleanup();
+            panic!("Error opening CSV file")
+        }
     };
     let filereader = BufReader::new(file);
     let mut csv_reader = ReaderBuilder::new()
@@ -41,54 +44,74 @@ fn process_transactions(filename: String) -> Result<(), Box<dyn Error>> {
         .has_headers(true)
         .from_reader(filereader);
     for result in csv_reader.deserialize::<Transaction>() {
-        let tx: Transaction = result?;
+        let mut tx: Transaction = result?;
         let mut acc: Account = match get_account(&ac_db, tx.client) {
             Ok(a) => match a {
                 Some(v) => v,
                 None => Account::new(tx.client),
             },
-            _ => panic!("Error handling accounts"),
+            _ => {
+                cleanup();
+                panic!("Error handling accounts")
+            }
         };
-        match tx.tx_type {
-            TxType::Deposit => tx.deposit(&mut acc),
-            TxType::Withdrawal => tx.withdrawal(&mut acc),
-            TxType::Dispute | TxType::Resolve | TxType::Chargeback => {
-                match get_transaction(&tx_db, tx.tx) {
-                    Ok(Some(mut updated_tx)) => {
-                        match tx.tx_type {
-                            TxType::Dispute => updated_tx.dispute(&mut acc),
-                            TxType::Resolve => updated_tx.resolve(&mut acc),
-                            TxType::Chargeback => updated_tx.chargeback(&mut acc),
-                            _ => unreachable!(),
-                        }
-                    },
-                    _ => continue,
+        match get_transaction(&tx_db, &tx.tx) {
+            Ok(Some(mut updated_tx)) => {
+                if tx.tx_type == updated_tx.tx_type {
+                    continue;
                 }
-            },
-            TxType::Unknown => continue,
+
+                match tx.tx_type {
+                    TxType::Deposit => tx.deposit(&mut acc),
+                    TxType::Withdrawal => tx.withdrawal(&mut acc),
+
+                    TxType::Dispute => {
+                        tx.tx.push_str("-d");
+                        updated_tx.dispute(&mut acc);
+                    }
+                    TxType::Resolve => {
+                        tx.tx.push_str("-r");
+                        updated_tx.resolve(&mut acc);
+                    }
+                    TxType::Chargeback => {
+                        tx.tx.push_str("-c");
+                        updated_tx.chargeback(&mut acc);
+                    }
+                }
+                insert_transaction(&tx_db, &updated_tx)?;
+            }
+            Ok(None) => {
+                match tx.tx_type {
+                    TxType::Deposit => tx.deposit(&mut acc),
+                    TxType::Withdrawal => tx.withdrawal(&mut acc),
+                    // If no original transaction is found for Dispute, Resolve, or Chargeback, skip
+                    TxType::Dispute | TxType::Resolve | TxType::Chargeback => continue,
+                }
+            }
+            Err(e) => {
+                eprintln!("Error processing transaction: {:?}", e);
+                continue;
+            }
         }
         insert_account(&ac_db, &acc)?;
         insert_transaction(&tx_db, &tx)?;
     }
     let _ = output_db_as_csv(&ac_db);
-    let _ = remove_dir_all(Account::DB_NAME);
-    let _ = remove_dir_all(Transaction::DB_NAME);
+    cleanup();
     Ok(())
 }
 
 fn insert_account(db: &Db, account: &Account) -> Result<(), Box<dyn Error>> {
     let serialized_data = to_string(account)?;
-    
+
     db.insert(account.id.to_be_bytes(), serialized_data.as_bytes())?;
-    
+
     db.flush()?;
     Ok(())
 }
 
 fn get_account(db: &Db, key: u16) -> Result<Option<Account>, Box<dyn Error>> {
-
     if let Some(serialized_data) = db.get(key.to_be_bytes())? {
-
         let account: Account = from_slice(&serialized_data)?;
         Ok(Some(account))
     } else {
@@ -98,16 +121,15 @@ fn get_account(db: &Db, key: u16) -> Result<Option<Account>, Box<dyn Error>> {
 
 fn insert_transaction(db: &Db, tx: &Transaction) -> Result<(), Box<dyn Error>> {
     let serialized_data = to_string(tx)?;
-    db.insert(tx.tx.to_be_bytes(), serialized_data.as_bytes())?;
-    
+    db.insert(tx.tx.as_bytes(), serialized_data.as_bytes())?;
+
     db.flush()?;
-    
+
     Ok(())
 }
 
-fn get_transaction(db: &Db, key: u32) -> Result<Option<Transaction>, Box<dyn Error>> {
-    if let Some(serialized_data) = db.get(key.to_be_bytes())? {
-
+fn get_transaction(db: &Db, key: &String) -> Result<Option<Transaction>, Box<dyn Error>> {
+    if let Some(serialized_data) = db.get(key.as_bytes())? {
         let tx: Transaction = from_slice(&serialized_data)?;
         Ok(Some(tx))
     } else {
@@ -133,4 +155,9 @@ fn output_db_as_csv(db: &Db) -> Result<(), Box<dyn Error>> {
 
     wtr.flush()?;
     Ok(())
+}
+
+fn cleanup() {
+    let _ = remove_dir_all(Account::DB_NAME);
+    let _ = remove_dir_all(Transaction::DB_NAME);
 }
--- src/transaction.rs
+++ src/transaction.rs
@@ -4,24 +4,27 @@ use crate::account::Account;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Transaction {
-    #[serde(rename="type")]
+    #[serde(rename = "type")]
     pub tx_type: TxType,
     pub client: u16,
-    pub tx: u32,
-    #[serde(deserialize_with="deserialize_amount")]
+    pub tx: String,
+    #[serde(deserialize_with = "deserialize_amount")]
     pub amount: f32,
-    #[serde(default="default_bool", deserialize_with="deserialize_dispute")]
-    under_dispute: bool,
+    #[serde(
+        default = "default_bool",
+        deserialize_with = "deserialize_dispute",
+        serialize_with = "bool_to_string"
+    )]
+    pub under_dispute: bool,
 }
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize, PartialEq)]
 pub enum TxType {
     Deposit,
     Withdrawal,
     Dispute,
     Resolve,
     Chargeback,
-    Unknown,
 }
 
 impl Transaction {
@@ -47,12 +50,19 @@ impl Transaction {
     }
 
     pub fn resolve(&mut self, acc: &mut Account) {
+        if !self.under_dispute {
+            println!("{:?}", self);
+            return;
+        }
         acc.available += self.amount;
         acc.held -= self.amount;
         self.under_dispute = false;
     }
 
     pub fn chargeback(&mut self, acc: &mut Account) {
+        if !self.under_dispute {
+            return;
+        }
         acc.total -= self.amount;
         acc.held -= self.amount;
         acc.locked = true;
@@ -63,21 +73,23 @@ impl Transaction {
 impl<'de> Deserialize<'de> for TxType {
     fn deserialize<D>(de: D) -> Result<Self, D::Error>
     where
-        D:Deserializer<'de> {
-            let variant = String::deserialize(de)?;
-            Ok(match variant.as_str() {
-                "deposit" => TxType::Deposit,
-                "withdrawal" => TxType::Withdrawal,
-                "dispute" => TxType::Dispute,
-                "resolve" => TxType::Resolve,
-                "chargeback" => TxType::Chargeback,
-                _ => TxType::Unknown,
-            })
+        D: Deserializer<'de>,
+    {
+        let variant = String::deserialize(de)?;
+        Ok(match variant.as_str() {
+            "Deposit" | "deposit" => TxType::Deposit,
+            "Withdrawal" | "withdrawal" => TxType::Withdrawal,
+            "Dispute" | "dispute" => TxType::Dispute,
+            "Resolve" | "resolve" => TxType::Resolve,
+            "Chargeback" | "chargeback" => TxType::Chargeback,
+            _ => panic!("Type variant unknown: {:?}", variant.as_str()),
+        })
     }
 }
 
-fn deserialize_amount<'de, D>(deserializer: D) -> Result<f32, D::Error> 
-where D:serde::Deserializer<'de>
+fn deserialize_amount<'de, D>(deserializer: D) -> Result<f32, D::Error>
+where
+    D: serde::Deserializer<'de>,
 {
     let s: Option<f32> = Option::deserialize(deserializer)?;
     match s {
@@ -86,14 +98,23 @@ where D:serde::Deserializer<'de>
     }
 }
 
-fn deserialize_dispute<'de, D>(deserializer: D) -> Result<bool, D::Error> 
-where D:serde::Deserializer<'de>
+fn deserialize_dispute<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: serde::Deserializer<'de>,
 {
-    let s: Option<bool> = Option::deserialize(deserializer)?;
+    let s: Option<String> = Option::deserialize(deserializer)?;
     match s {
-        Some(b) => Ok(b),
+        Some(b) => Ok(b.parse().map_err(serde::de::Error::custom))?,
         None => Ok(false),
     }
+}
+
+fn bool_to_string<S>(value: &bool, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let s = if *value { "true" } else { "false" };
+    serializer.serialize_str(s)
 }
 
 fn default_bool() -> bool {
