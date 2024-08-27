ba1fe82d7a752d5694f059e492e7a1dda6256655
Paulo Moreira <43989852+paultergust@users.noreply.github.com>
2024-08-27 03:58:31 +0100

    Dev (#2)
    * Implement core tx processing
    
    * Implement code on main() to read csv file
    
    * Implement db connections and pattern-matching to process txs
    
    * Change db serialization from bincode to serde_json
    
    * Fix bug where the under_dispute flag was not toggling
    
    * Refactor and add some comments
    
    * Set amount to abs() when parsing, to avoid negative operations

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-mod transaction;
 mod account;
+mod transaction;
 
 use std::error::Error;
-use std::fs::remove_dir_all;
-use std::{fs::File, io::BufReader};
+use std::fs::{remove_dir_all, File};
+use std::io::BufReader;
 
 use clap::Parser;
 use csv::{ReaderBuilder, Trim, Writer};
-use sled::Db;
 use serde_json::{from_slice, to_string};
+use sled::Db;
 use transaction::TxType;
 
-use crate::transaction::Transaction;
 use crate::account::Account;
+use crate::transaction::Transaction;
 
 #[derive(Parser)]
 struct Cli {
@@ -22,73 +22,103 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     let filepath = cli.filepath;
-    match process_transactions(filepath) {
-        Ok(()) => (),
-        Err(e) => eprintln!("Error processing transactions: {}", e),
-    };
+
+    if let Err(e) = process_transactions(filepath) {
+        eprintln!("Error processing transactions: {}", e);
+    }
 }
 
 fn process_transactions(filename: String) -> Result<(), Box<dyn Error>> {
+    // two different K/V databases, to hold Accounts and Transactions on disk instead of in memory,
+    // in a somewhat "hashmap" fashion
     let tx_db = sled::open(Transaction::DB_NAME)?;
     let ac_db = sled::open(Account::DB_NAME)?;
-    let file = match File::open(filename) {
-        Ok(v) => v,
-        _ => panic!("error opening file"),
-    };
+
+    let file = File::open(&filename).map_err(|_| "Error opening CSV file")?;
+    // Use buffreader so the file is not loaded in memory all at once
     let filereader = BufReader::new(file);
     let mut csv_reader = ReaderBuilder::new()
         .trim(Trim::All)
         .has_headers(true)
         .from_reader(filereader);
+
     for result in csv_reader.deserialize::<Transaction>() {
-        let tx: Transaction = result?;
-        let mut acc: Account = match get_account(&ac_db, tx.client) {
-            Ok(a) => match a {
-                Some(v) => v,
-                None => Account::new(tx.client),
-            },
-            _ => panic!("Error handling accounts"),
-        };
-        match tx.tx_type {
-            TxType::Deposit => tx.deposit(&mut acc),
-            TxType::Withdrawal => tx.withdrawal(&mut acc),
-            TxType::Dispute | TxType::Resolve | TxType::Chargeback => {
-                match get_transaction(&tx_db, tx.tx) {
-                    Ok(Some(mut updated_tx)) => {
-                        match tx.tx_type {
-                            TxType::Dispute => updated_tx.dispute(&mut acc),
-                            TxType::Resolve => updated_tx.resolve(&mut acc),
-                            TxType::Chargeback => updated_tx.chargeback(&mut acc),
-                            _ => unreachable!(),
-                        }
-                    },
-                    _ => continue,
-                }
-            },
-            TxType::Unknown => continue,
+        let mut tx: Transaction = result?;
+        let mut acc = get_or_create_account(&ac_db, tx.client)?;
+
+        match process_transaction(&tx_db, &mut acc, &mut tx) {
+            Ok(()) => {
+                insert_account(&ac_db, &acc)?;
+            }
+            Err(e) => eprintln!("Error processing transaction: {}", e),
         }
-        insert_account(&ac_db, &acc)?;
-        insert_transaction(&tx_db, &tx)?;
     }
-    let _ = output_db_as_csv(&ac_db);
-    let _ = remove_dir_all(Account::DB_NAME);
-    let _ = remove_dir_all(Transaction::DB_NAME);
+
+    output_db_as_csv(&ac_db)?;
+    cleanup();
     Ok(())
+}
+
+fn process_transaction(
+    tx_db: &Db,
+    acc: &mut Account,
+    tx: &mut Transaction,
+) -> Result<(), Box<dyn Error>> {
+    match get_transaction(tx_db, &tx.tx)? {
+        Some(mut updated_tx) => {
+            if tx.tx_type == updated_tx.tx_type && tx.amount == updated_tx.amount {
+                return Ok(()); // Idempotent transaction, nothing to do
+            }
+
+            // adding suffix to tx so they don't overwrite Deposits and Withdrawals,
+            // which can be disputed later
+            tx.tx.push_str(match tx.tx_type {
+                TxType::Dispute => "-d",
+                TxType::Resolve => "-r",
+                TxType::Chargeback => "-c",
+                _ => "",
+            });
+
+            match tx.tx_type {
+                TxType::Deposit => tx.deposit(acc),
+                TxType::Withdrawal => tx.withdrawal(acc),
+                TxType::Dispute => updated_tx.dispute(acc),
+                TxType::Resolve => updated_tx.resolve(acc),
+                TxType::Chargeback => updated_tx.chargeback(acc),
+            }
+
+            insert_transaction(tx_db, &updated_tx)?;
+        }
+        None => {
+            match tx.tx_type {
+                TxType::Deposit => tx.deposit(acc),
+                TxType::Withdrawal => tx.withdrawal(acc),
+                TxType::Dispute | TxType::Resolve | TxType::Chargeback => return Ok(()),
+            }
+            insert_transaction(tx_db, &tx)?;
+        }
+    }
+    Ok(())
+}
+
+fn get_or_create_account(db: &Db, client_id: u16) -> Result<Account, Box<dyn Error>> {
+    // for each transaction, one account fetched or created
+    // check if transaction with same tx (id) already stored
+    match get_account(db, client_id)? {
+        Some(acc) => Ok(acc),
+        None => Ok(Account::new(client_id)),
+    }
 }
 
 fn insert_account(db: &Db, account: &Account) -> Result<(), Box<dyn Error>> {
     let serialized_data = to_string(account)?;
-    
     db.insert(account.id.to_be_bytes(), serialized_data.as_bytes())?;
-    
     db.flush()?;
     Ok(())
 }
 
 fn get_account(db: &Db, key: u16) -> Result<Option<Account>, Box<dyn Error>> {
-
     if let Some(serialized_data) = db.get(key.to_be_bytes())? {
-
         let account: Account = from_slice(&serialized_data)?;
         Ok(Some(account))
     } else {
@@ -98,16 +128,13 @@ fn get_account(db: &Db, key: u16) -> Result<Option<Account>, Box<dyn Error>> {
 
 fn insert_transaction(db: &Db, tx: &Transaction) -> Result<(), Box<dyn Error>> {
     let serialized_data = to_string(tx)?;
-    db.insert(tx.tx.to_be_bytes(), serialized_data.as_bytes())?;
-    
+    db.insert(tx.tx.as_bytes(), serialized_data.as_bytes())?;
     db.flush()?;
-    
     Ok(())
 }
 
-fn get_transaction(db: &Db, key: u32) -> Result<Option<Transaction>, Box<dyn Error>> {
-    if let Some(serialized_data) = db.get(key.to_be_bytes())? {
-
+fn get_transaction(db: &Db, key: &String) -> Result<Option<Transaction>, Box<dyn Error>> {
+    if let Some(serialized_data) = db.get(key.as_bytes())? {
         let tx: Transaction = from_slice(&serialized_data)?;
         Ok(Some(tx))
     } else {
@@ -122,15 +149,21 @@ fn output_db_as_csv(db: &Db) -> Result<(), Box<dyn Error>> {
 
     for result in db.iter() {
         let (_, value) = result?;
-
         let account: Account = from_slice(&value)?;
-        let fmt_av = format!("{:.4}", account.available);
-        let fmt_hd = format!("{:.4}", account.held);
-        let fmt_tt = format!("{:.4}", account.total);
-
-        wtr.serialize((account.id, fmt_av, fmt_hd, fmt_tt, account.locked))?;
+        wtr.serialize((
+            account.id,
+            format!("{:.4}", account.available),
+            format!("{:.4}", account.held),
+            format!("{:.4}", account.total),
+            account.locked,
+        ))?;
     }
 
     wtr.flush()?;
     Ok(())
+}
+
+fn cleanup() {
+    let _ = remove_dir_all(Account::DB_NAME);
+    let _ = remove_dir_all(Transaction::DB_NAME);
 }
